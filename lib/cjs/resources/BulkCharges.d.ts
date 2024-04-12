150c229e6dd75aed9042995e3898ca57f52b14f4
Obi Pascal <pascalobi83@gmail.com>
2024-04-12 11:43:18 +0100

    updated

--- lib/cjs/resources/BulkCharges.d.ts
+++ lib/cjs/resources/BulkCharges.d.ts
@@ -37,3 +37,41 @@ export default class BulkCharges {
      */
     resume(batch_code: string): Promise<import("axios").AxiosResponse<any, any>>;
 }
+export interface BulkChargesApi {
+    /**
+     * The Bulk Charges API allows you create and manage multiple recurring payments from your customers.
+     * @param apiKey Paystack API key
+     */
+    constructor(apiKey: string): BulkChargesApi;
+    /**
+     * Send an array of objects with authorization codes and amount, using the supported currency format, so we can process transactions as a batch.
+     * @param data Bulk charge details
+     */
+    create(data: Record<string, BulkChargeBody[]>): Promise<any>;
+    /**
+     * This lists all bulk charge batches created by the integration. Statuses can be active, paused, or complete
+     * @param params Pagination parameters (optional)
+     */
+    list(params?: PaginationParams): Promise<any>;
+    /**
+     * This endpoint retrieves a specific batch code. It also returns useful information on its progress by way of the total_charges and pending_charges attributes.
+     * @param id_or_code Bulk charge ID or code
+     */
+    fetch(id_or_code: string): Promise<any>;
+    /**
+     * This endpoint retrieves the charges associated with a specified batch code. Pagination parameters are available. You can also filter by status. Charge statuses can be pending, success or failed.
+     * @param id_or_code Bulk charge ID or code
+     * @param params Pagination parameters (optional)
+     */
+    fetchBatch(id_or_code: string, params?: PaginationParams): Promise<any>;
+    /**
+     * Use this endpoint to pause processing a batch
+     * @param batch_code Batch code of the bulk charge
+     */
+    pause(batch_code: string): Promise<any>;
+    /**
+     * Use this endpoint to resume processing a batch
+     * @param batch_code Batch code of the bulk charge
+     */
+    resume(batch_code: string): Promise<any>;
+}
--- lib/es/types/index.d.ts
+++ lib/es/types/index.d.ts
@@ -1,3 +1,9 @@
+import { AxiosResponse } from "axios";
+import { ApplePayBody } from "./applepay";
+import { PaginationParams } from "./const";
+import { BulkChargeBody, ChargeBody, SubmitAddressBody, SubmitBirthdayBody, SubmitOtpBody, SubmitPINBody, SubmitPhoneBody } from "./charge";
+import { BlacklistOrWhitelistCustomerBody, CreateCustomerBody, DeactivateAuthorizationBody, UpdateCustomerBody } from "./customer";
+import { AssignDedicatedVirtualAccountBody, CreateDedicatedVirtualAccountBody, DedicatedVirtualAccountQueryParams, DedicatedVirtualAccountSplitTransactionBody, RemoveSplitDedicatedVirtualAccountBody } from "./dedicatedVirtualAccount";
 export * from "./applepay";
 export * from "./charge";
 export * from "./const";
@@ -16,3 +22,199 @@ export * from "./transactions";
 export * from "./transfer";
 export * from "./transferRecipient";
 export * from "./verification";
+export interface ApplePayApi {
+    /**
+     * The Apple Pay API allows you register your application's top-level domain or subdomain.
+     * @param apiKey Paystack API key
+     */
+    constructor(apiKey: string): ApplePayApi;
+    /**
+     * Register a top-level domain or subdomain for your Apple Pay integration.
+     * @param data Domain name to be registered
+     */
+    registerDomain(data: ApplePayBody): Promise<AxiosResponse<any, any>>;
+    /**
+     * Lists all registered domains on your integration. Returns an empty array if no domains have been added.
+     */
+    listDomains(): Promise<AxiosResponse<any, any>>;
+    /**
+     * Unregister a top-level domain or subdomain previously used for your Apple Pay integration.
+     * @param data request body
+     */
+    unregisterDomain(data: ApplePayBody): Promise<AxiosResponse<any, any>>;
+}
+export interface BulkChargesApi {
+    /**
+     * The Bulk Charges API allows you create and manage multiple recurring payments from your customers.
+     * @param apiKey Paystack API key
+     */
+    constructor(apiKey: string): BulkChargesApi;
+    /**
+     * Send an array of objects with authorization codes and amount, using the supported currency format, so we can process transactions as a batch.
+     * @param data Bulk charge details
+     */
+    create(data: Record<string, BulkChargeBody[]>): Promise<AxiosResponse<any, any>>;
+    /**
+     * This lists all bulk charge batches created by the integration. Statuses can be active, paused, or complete
+     * @param params Pagination parameters (optional)
+     */
+    list(params?: PaginationParams): Promise<AxiosResponse<any, any>>;
+    /**
+     * This endpoint retrieves a specific batch code. It also returns useful information on its progress by way of the total_charges and pending_charges attributes.
+     * @param id_or_code Bulk charge ID or code
+     */
+    fetch(id_or_code: string): Promise<AxiosResponse<any, any>>;
+    /**
+     * This endpoint retrieves the charges associated with a specified batch code. Pagination parameters are available. You can also filter by status. Charge statuses can be pending, success or failed.
+     * @param id_or_code Bulk charge ID or code
+     * @param params Pagination parameters (optional)
+     */
+    fetchBatch(id_or_code: string, params?: PaginationParams): Promise<AxiosResponse<any, any>>;
+    /**
+     * Use this endpoint to pause processing a batch
+     * @param batch_code Batch code of the bulk charge
+     */
+    pause(batch_code: string): Promise<AxiosResponse<any, any>>;
+    /**
+     * Use this endpoint to resume processing a batch
+     * @param batch_code Batch code of the bulk charge
+     */
+    resume(batch_code: string): Promise<AxiosResponse<any, any>>;
+}
+export interface ChargesApi {
+    /**
+     * The Charge API allows you to configure payment channel of your choice when initiating a payment.
+     * @param apiKey Paystack API key
+     */
+    constructor(apiKey: string): ChargesApi;
+    /**
+     * Initialize a charge
+     * @param data Charge details
+     */
+    initialize(data: ChargeBody): Promise<AxiosResponse<any, any>>;
+    /**
+     * Submit a PIN
+     * @param data Charge details
+     */
+    submitPin(data: SubmitPINBody): Promise<AxiosResponse<any, any>>;
+    /**
+     * Submit an OTP
+     * @param data Charge details
+     */
+    submitOtp(data: SubmitOtpBody): Promise<AxiosResponse<any, any>>;
+    /**
+     * Submit a phone number
+     * @param data Charge details
+     */
+    submitPhone(data: SubmitPhoneBody): Promise<AxiosResponse<any, any>>;
+    /**
+     * Submit a birthday
+     * @param data Charge details
+     */
+    submitBirthday(data: SubmitBirthdayBody): Promise<AxiosResponse<any, any>>;
+    /**
+     * Check pending charge
+     * @param reference Charge reference
+     */
+    checkPendingCharge(reference: string): Promise<AxiosResponse<any, any>>;
+    /**
+     * Submit an address
+     * @param data Charge details
+     */
+    submitAddress(data: SubmitAddressBody): Promise<AxiosResponse<any, any>>;
+}
+export interface CustomersApi {
+    /**
+     * The Customers API allows you create and manage customers on your integration.
+     * @param apiKey Paystack API key
+     */
+    constructor(apiKey: string): CustomersApi;
+    /**
+     * Create a customer on your integration.
+     * ___
+     * The first_name, last_name and phone are optional parameters. However, when creating a customer that would be assigned a Dedicated Virtual Account and your business catgeory falls under Betting, Financial services, and General Service, then these parameters become compulsory.
+     * @param data Customer data
+     */
+    create(data: CreateCustomerBody): Promise<AxiosResponse<any, any>>;
+    /**
+     * List customers available on your integration.
+     */
+    list(): Promise<AxiosResponse<any, any>>;
+    /**
+     * Get details of a customer on your integration.
+     * @param email_or_code Customer's email or code
+     */
+    fetch(email_or_code: string): Promise<AxiosResponse<any, any>>;
+    /**
+     * Update a customer's details on your integration
+     * @param code Customer's code
+     * @param data Customer data
+     */
+    update(code: string, data: UpdateCustomerBody): Promise<AxiosResponse<any, any>>;
+    /**
+     * Validate a customer's identity
+     * @param code Customer's code
+     * @param data Customer identification data
+     */
+    validate(code: string, data: any): Promise<AxiosResponse<any, any>>;
+    /**
+     * Whitelist or blacklist a customer on your integration
+     * @param data Customer risk action data
+     */
+    blacklistOrWhitelistCustomer(data: BlacklistOrWhitelistCustomerBody): Promise<AxiosResponse<any, any>>;
+    /**
+     * Deactivate an authorization
+     * @param data Authorization deactivation data
+     */
+    deactivateAuthorization(data: DeactivateAuthorizationBody): Promise<AxiosResponse<any, any>>;
+}
+export interface DedicatedVirtualAccountsApi {
+    /**
+     * The Dedicated Virtual Account API enables Nigerian merchants to manage unique payment accounts of their customers.
+     * @param apiKey Paystack API key
+     */
+    constructor(apiKey: string): DedicatedVirtualAccountsApi;
+    /**
+     * Create a dedicated virtual account
+     * @param data Account data
+     */
+    create(data: CreateDedicatedVirtualAccountBody): Promise<AxiosResponse<any, any>>;
+    /**
+     * Assign a dedicated virtual account to a customer
+     * @param data Account data
+     */
+    assign(data: AssignDedicatedVirtualAccountBody): Promise<AxiosResponse<any, any>>;
+    /**
+     * List dedicated virtual accounts available on your integration.
+     */
+    list(): Promise<AxiosResponse<any, any>>;
+    /**
+     * Get details of a dedicated virtual account on your integration.
+     * @param dedicated_account_id Dedicated virtual account ID
+     */
+    fetch(dedicated_account_id: string): Promise<AxiosResponse<any, any>>;
+    /**
+     * Requery Dedicated Virtual Account for new transactions
+     * @param data Query data
+     */
+    query(data: DedicatedVirtualAccountQueryParams): Promise<AxiosResponse<any, any>>;
+    /**
+     * Deactivate a dedicated virtual account on your integration.
+     * @param dedicated_account_id Dedicated virtual account ID
+     */
+    deactivate(dedicated_account_id: string): Promise<AxiosResponse<any, any>>;
+    /**
+     * Split a dedicated virtual account transaction with one or more accounts
+     * @param data Transaction data
+     */
+    split(data: DedicatedVirtualAccountSplitTransactionBody): Promise<AxiosResponse<any, any>>;
+    /**
+     * If you've previously set up split payment for transactions on a dedicated virtual account, you can remove it with this endpoint
+     * @param data Transaction data
+     */
+    removeSplit(data: RemoveSplitDedicatedVirtualAccountBody): Promise<AxiosResponse<any, any>>;
+    /**
+     * Get available bank providers for a dedicated virtual account
+     */
+    fetchBankProviders(): Promise<AxiosResponse<any[], any>>;
+}
