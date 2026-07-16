167cc4ca6f9cd4d45d94f8a9b1fb283314859c68
Obi Pascal <pascalobi83@gmail.com>
2026-07-16 13:52:29 +0100

    feat: add source field to TransferBody and omit it from bulk transfers

--- lib/cjs/types/transfer.d.ts
+++ lib/cjs/types/transfer.d.ts
@@ -1,6 +1,8 @@
 import { AxiosInstance, AxiosResponse } from "axios";
 import { Currencies } from "./const";
 export type TransferBody = {
+    /** The source to be debited. Only balance is supported */
+    source: "balance";
     /** Amount to transfer in kobo if currency is NGN and pesewas if currency is GHS. */
     amount: string | number;
     /** If specified, the field should be a unique identifier (in lowercase) for the object. Only -,_ and alphanumeric characters allowed. */
@@ -22,7 +24,7 @@ export type BulkTransferBody = {
     /** Currency of the transfer */
     currency: Currencies;
     /** A list of transfer object. Each object should contain amount, recipient, and reference */
-    transfers: Array<TransferBody>;
+    transfers: Array<Omit<TransferBody, "source">>;
 };
 export type ResendTranasferCodeBody = {
     /** Transfer code */
